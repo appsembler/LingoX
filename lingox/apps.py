81d35ecd8aaf4a25f7fe855c393f5d64e4a0a0b1
Omar Al-Ithawi <i@omardo.com>
2018-05-23 04:44:02 +0300

    Cookie cutter for LingoX

---
+++ lingox/apps.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+lingox Django application initialization.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class LingoXConfig(AppConfig):
+    """
+    Configuration for the lingox Django application.
+    """
+
+    name = 'lingox'
---
+++ tests/test_models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests for the `lingox` models module.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+def test_fake():
+    """
+    Dummy test.
+    """
+    assert True
